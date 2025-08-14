52ee19074c6de490061e4dceca279a9e8c44a919
adsforfamebooster <153657573+adsforfamebooster@users.noreply.github.com>
2025-08-14 16:04:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,8 @@ async def approve(_, m : Message):
 @app.on_message(filters.command("start"))
 async def op(_, m :Message):
     try:
-        await app.get_chat_member(cfg.CHID, m.from_user.id) 
+        await app.get_chat(cfg.CHID)  # <-- New line to cache the chat
+        await app.get_chat_member(cfg.CHID, m.from_user.id)
         if m.chat.type == enums.ChatType.PRIVATE:
             keyboard = InlineKeyboardMarkup(
                 [
